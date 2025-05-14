6065d9e048d087d2b3e82ed7ce40ee938b19ad56
Ryan Welcher <me@ryanwelcher.com>
2025-05-14 15:06:45 -0400

    Card image and css changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Card.css";
 
 const Card = ({ word, isFlipped, onClick }) => {
+  useEffect(() => {
+    if (isFlipped) {
+      const speech = new SpeechSynthesisUtterance(word);
+      window.speechSynthesis.speak(speech);
+    }
+  }, [isFlipped, word]);
+
+  const image =
+    "https://resizing.flixster.com/eGfSs7-RN8wAxu2DQqcqmCR-rbk=/150x200/v2/https://resizing.flixster.com/ViCYuISBvkrLsqD3ePYNSHszhoc=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzLzc1ZTAwYzViLTIyMzAtNDJmMS04NjNlLTBjMTAyMjkyZDhhYy5qcGc=";
+
+  const creeper =
+    "https://m.media-amazon.com/images/I/31olIWRlnCL._AC_SY1000_.jpg";
+
   return (
     <div className={`card ${isFlipped ? "flipped" : ""}`} onClick={onClick}>
       <div className="card-inner">
         <div className="card-front">
-          <img
-            src="https://placehold.co/150x200/orange/white"
-            alt="Card Back"
-          />
+          <img src={creeper} alt="Card Back" width="120" />
         </div>
         <div className="card-back">{word}</div>
       </div>
