08637db4dd5b6dec43a3c15dfdf39ac2ee6cedff
Ryan Welcher <me@ryanwelcher.com>
2025-05-31 21:36:12 -0400

    Fix card flip issues.

--- src/App.js
+++ src/App.js
@@ -161,7 +161,6 @@ function App() {
               <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" />
             </svg>
           </button>
-          <h2>Level {getLevelNumber(selectedLevel)}</h2>
           <h3>
             Difficulty:{" "}
             {difficulty.charAt(0).toUpperCase() + difficulty.slice(1)}
